af70defc3d52b919a833f27d360dedb5c4709c39
Ben Tilghman <ben.tilghman@gmail.com>
2019-06-21 20:01:23 -0400

    can add user to queue and repopulate queue wo user

--- src/router/peopleRouter.js
+++ src/router/peopleRouter.js
@@ -1,22 +1,44 @@
 'use strict';
 
+const Queue = require('../queue');
 const express = require('express');
 const { people }  = require('../data/peopleData');
 
 const peopleRouter = express.Router();
 
+const rePeople = new Queue();
+const user = 'You';
+
 peopleRouter
   .route('/api/people')
   .get((req, res) => {
+    
+    return res.json(people);
+  })
+
+peopleRouter
+  .route('/api/people/add')
+  .post((req, res) => {
+    people.enqueue({name: user})
+
     return res.json(people);
   })
 
 peopleRouter
   .route('/api/people/delete')
   .delete((req, res) => {
-    people.enqueue(
-      people.dequeue()
-    )
+    
+    if (people.first.value.name === user) {
+      people.dequeue();
+    } else{
+      rePeople.enqueue(people.dequeue())
+    }
+
+    if (!people.first) {
+      while (rePeople.first !== null) {
+        people.enqueue(rePeople.dequeue());
+      }
+    }
     return res.json(people);
   })
 
